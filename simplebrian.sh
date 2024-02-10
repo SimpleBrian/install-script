f877ed861f7cd379cc481a28b3c61b7f3387c3e3
SimpleBrian <48006102+SimpleBrian@users.noreply.github.com>
2024-02-10 21:00:57 +1100

    separate pacman packages and AUR packages.
    now they're both run as separate commands. still keeping the paru command instead of reverting to "sudo pacman -S ..." because the "--sudoloop" flag is too useful to lose, as it constantly refreshes its sudo session.

--- simplebrian.sh
+++ simplebrian.sh
@@ -36,7 +36,9 @@ cd
 
 # download package list and use paru to install everything (using as pacman wrapper on top of AUR packages).
 wget https://github.com/SimpleBrian/install-script/raw/main/packages.txt
+wget https://github.com/SimpleBrian/install-script/raw/main/aur_packages.txt
 paru -Syu --needed --sudoloop --noconfirm - < packages.txt
+paru -Syu --needed --sudoloop --noconfirm - < aur_packages.txt
 
 # make terminal hella fancy (and configure colors). must fix vte config later.
 git clone --recursive https://github.com/andresgongora/synth-shell.git
